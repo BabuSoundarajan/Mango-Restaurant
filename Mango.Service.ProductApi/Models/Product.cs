1108e0fae0d14abfb83c94c8c7e2a0763340b65c
BabuSoundarajan <babusaran11@gmail.com>
2023-03-07 20:53:17 +0530

    Initial commit

--- Mango.Service.ProductApi/Models/Product.cs
+++ Mango.Service.ProductApi/Models/Product.cs
@@ -12,5 +12,6 @@ namespace Mango.Service.ProductApi.Models
         public double Price { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
+        public string CategoryName { get; set; }
     }
 }
